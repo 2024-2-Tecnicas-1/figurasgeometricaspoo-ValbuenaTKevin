af5c6e93284704f3f0efa9323356f52be673a247
Kevin <valbuenatkevin@gmai.com>
2024-10-20 20:56:40 -0500

    Errores
    Corrección de errores pt 1

--- src/main/java/com/mycompany/figurasgeometricas/FigurasGeometricasPrincipal.java
+++ src/main/java/com/mycompany/figurasgeometricas/FigurasGeometricasPrincipal.java
@@ -5,39 +5,39 @@ import java.util.Scanner;
 public class FigurasGeometricasPrincipal {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner lector = new Scanner(System.in);
         String nombre, color;
         double radio, lado1, lado2, base, altura;
         char tecla = ' ';
 
         System.out.println("Ingrese el nombre de la figura");
-        nombre = sc.next();
+        nombre = lector.next();
         System.out.println("Ingrese el color de la figura");
-        color = sc.next();
+        color = lector.next();
         System.out.println("Ingrese el tipo de figura");
         System.out.println("1: Circulo");
         System.out.println("2: Rectangulo");
         System.out.println("3: Triangulo");
-        tecla = sc.next().charAt(0);
+        tecla = lector.next().charAt(0);
         tecla = Character.toLowerCase(tecla);
 
         switch (tecla) {
             case '1':
 
-                System.out.println("Ingrese el radio del circulo");
-                radio = sc.nextDouble();
+                System.out.println("Ingrese el radio del círculo");
+                radio = lector.nextDouble();
 
                 Circulo circulo = new Circulo(nombre, color, radio);
 
-                System.out.println("El area del circulo " + circulo.getNombre()+ " de color " + circulo.getColor()+ " es: "+circulo.obtenerArea());
-                System.out.println("El perimentro del circulo " + circulo.getNombre()+ " de color " + circulo.getColor()+ " es: "+circulo.obtenerPerimetro());
+                System.out.println("El área del circulo " + circulo.getNombre()+ " de color " + circulo.getColor()+ " es: "+circulo.obtenerArea());
+                System.out.println("El perímetro del circulo " + circulo.getNombre()+ " de color " + circulo.getColor()+ " es: "+circulo.obtenerPerimetro());
 
                 break;
             case '2':
                 System.out.println("Ingrese el valor del lado 1 del rectangulo");
-                lado1 = sc.nextDouble();
+                lado1 = lector.nextDouble();
                 System.out.println("Ingrese el valor del lado 2 del rectangulo");
-                lado2 = sc.nextDouble();
+                lado2 = lector.nextDouble();
 
                 Rectangulo rectangulo = new Rectangulo(nombre, color, lado1, lado2);
 
@@ -46,9 +46,9 @@ public class FigurasGeometricasPrincipal {
                 break;
             case '3':
                 System.out.println("Ingrese el valor de la base del triángulo");
-                base = sc.nextDouble();
+                base = lector.nextDouble();
                 System.out.println("Ingrese el valor de la altura del triángulo");
-                altura = sc.nextDouble();
+                altura = lector.nextDouble();
 
                 Triangulo triangulo = new Triangulo(nombre, color, base, altura);
 
--- src/main/java/com/mycompany/figurasgeometricas/Principal.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.figurasgeometricas;
-
-public class Principal {
-
-    public static void main(String[] args) {
-        // TODO: Tu código va aquí
-    }
-}
